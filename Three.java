eaeca93eb40d08254456e3137a656cd055fa4871
unknown <rajeshwarip.xworkz@gmail.com>
2024-06-26 23:03:34 +0530

    Multiple of three

---
+++ Three.java
@@ -0,0 +1,15 @@
+public class Three{
+   public static void main(String args[]){
+       int num[]={3,4,3,20,18,30};
+	  
+	   for(int x=0;x<num.length;x++)
+	   {
+	     if( num[x]%3 == 0)
+		 {
+		   System.out.println("multiple of three are :"+ num[x]);		   
+		 }
+		 		 
+	   }
+	   
+	}
+}
