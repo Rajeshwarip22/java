c7bd7d7deede6499b00412623e1e2b31c17d913d
unknown <rajeshwarip.xworkz@gmail.com>
2024-06-26 23:02:30 +0530

    Reversing the array elements

---
+++ ReverseOfArrayElem.java
@@ -0,0 +1,11 @@
+public class ReverseOfArrayElem{
+  public static void main(String args[]){
+      int mulOfFive[]={5,10,15,20,25,30,35,40,45,50};
+	  
+	  System.out.println("Reverse order of array : ");
+	  for(int i = mulOfFive.length - 1; i>=0; i--)
+	  {
+	    System.out.print(mulOfFive[i]+ " ");
+	  }
+	 }
+}
