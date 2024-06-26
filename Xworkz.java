bc78af9e37dc620b2200bebb9f7a026cc99660fc
unknown <rajeshwarip.xworkz@gmail.com>
2024-06-26 20:42:15 +0530

    array 25-06-2024

---
+++ Xworkz.java
@@ -0,0 +1,35 @@
+public class  Xworkz{
+   public static void main(String args[]){
+       String students[]={"abc","xyz","ijk"};
+	   int score[]={502,547,630};
+	   long phno[]={123654789l,7531598246l,1235647896l};
+	   char section[]={'a','b','c'};
+	   
+	   System.out.print("students are : ");
+	   for(int i=0;i<students.length;i++)
+	   {
+	    System.out.print(students[i]+ " ");
+	   }
+	   System.out.println("\n");
+	   
+	   System.out.print("scores are : ");
+	   for(int i=0;i<score.length;i++)
+	   {
+	    System.out.print(score[i]+ " ");
+	   }
+	   System.out.println("\n");
+	   
+	   System.out.print("phno are : ");
+	   for(int i=0;i<phno.length;i++)
+	   {
+	    System.out.print(phno[i]+ " ");
+	   }
+	   System.out.println("\n");
+	   
+	   System.out.print("sections are : ");
+	   for(int i=0;i<section.length;i++)
+	   {
+	    System.out.print(section[i]+ " ");
+	   }
+	  }
+	 }
