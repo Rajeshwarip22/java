3a9f4b8b37418307ced7461857c9735efc28eb7a
unknown <rajeshwarip.xworkz@gmail.com>
2024-06-26 23:05:15 +0530

    accessing static array from another class

---
+++ Tree.java
@@ -0,0 +1,4 @@
+public class Tree{
+    public static  String location[]={"btm","haveri","hubli"};
+    public static  String soilRequired[]={"black","red","clay"};
+}
---
+++ TreeRunner.java
@@ -0,0 +1,12 @@
+public class TreeRunner{
+    public static void main(String args[]){
+	    for(int i = 0;i<Tree.location.length;i++)
+		{
+		  System.out.println("locations are : "+Tree.location[i]);
+		 }
+		 for(int i = 0;i<Tree.soilRequired.length;i++)
+		{
+		  System.out.println("soil requirement are : "+Tree.soilRequired[i]);
+		}
+		}
+	}
