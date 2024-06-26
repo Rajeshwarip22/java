24c1a718d6d6f2fd3c40a646b442ccdfafd3c668
unknown <rajeshwarip.xworkz@gmail.com>
2024-06-26 23:00:44 +0530

    Sum of array elements

---
+++ Sum.java
@@ -0,0 +1,12 @@
+public class Sum{
+  public static void main(String args[]){
+      int mulOfFive[]={5,10,15};
+	  int sum=0;
+	  
+	  for(int i =0;i<mulOfFive.length;i++)
+	  {
+	    sum=sum+mulOfFive[i];
+	  }
+	  System.out.println("Sum of array elements : "+sum);
+	 }
+}
