0f42514bdb230f54b3b0e4644720b11aa51cff59
unknown <rajeshwarip.xworkz@gmail.com>
2024-06-27 09:24:24 +0530

    display number from 1 to 100

---
+++ NumberFlow.java
@@ -0,0 +1,10 @@
+public class NumberFlow{
+   public static void main(String args[])
+   {
+     System.out.println("numbers : ");
+     for(int i = 1;i<=100;i++)
+	 {
+	   System.out.print(i + " ");
+	  }
+	}
+}
